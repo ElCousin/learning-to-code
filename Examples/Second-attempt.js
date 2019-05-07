150b4ea57ff2d6cd911dd5fa66a6b5a7a4b4aaf6
Cousin <ivan.vreyes@gmail.com>
2019-05-06 19:37:47 -0500

    Segundo intento con clases (objetos)
    Hubo un error con el parámetro que se llamaba igual que la clase (ambos CPU). Se cambia el nombre y ya ejecuta el programa.

--- Examples/Second-attempt.js
+++ Examples/Second-attempt.js
@@ -1,18 +1,4 @@
-class specsPC
-{
-    constructor(MOBO,CPU,RAM,PSU,GPU,SSD,Case)
-    {
-        this.tarjeta = MOBO
-        this.Proce = new CPU
-        this.memoria = RAM
-        this.poder = PSU
-        this.video = GPU
-        this.storage = SSD
-        this.gabinete = Case  
-    }
-}
-
-class CPU
+class Proce
 {
     constructor() //¿Por qué los componentes del constructor quedan en blanco?
     {
@@ -23,7 +9,24 @@ class CPU
         this.socket = undefined
         this.TDP = undefined
     }
-}   
+} 
+
+// class Proce{}
+
+class specsPC
+{
+    constructor(MOBO,CPU,RAM,PSU,GPU,SSD,Case)
+    {
+        this.tarjeta = MOBO//this es para objetos 
+        this.Proce = new Proce
+        this.memoria = RAM
+        this.poder = PSU
+        this.video = GPU
+        this.storage = SSD
+        this.gabinete = Case  
+    }
+}
+
 
 let gamerPC = new specsPC("Algo")
 console.log(gamerPC)
